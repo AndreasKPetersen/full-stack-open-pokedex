ba541305d95764741eeb5f2b2f8be0661a35ddff
Andreas K. Petersen <AndreasKPetersen@hotmail.com>
2023-08-25 14:36:52 +0200

    11.11 Health check and rollback

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.get("/health", (req, res) => {
   res.send("ok")
 })
 
+app.get("/version", (req, res) => {
+  res.send("6") // change this string to ensure a new version deployed
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log("server started on port 8080")
